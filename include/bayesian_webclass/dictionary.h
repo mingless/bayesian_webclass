bb9eefabef888833c53a34685d43918b46056803
mingless <mateusz.dziwulski@gmail.com>
2017-01-16 19:08:52 +0100

    Cleaned up and added functionality to Dictionary.
    Dictionary now implements methods to fetch word list, string and
    compare the string against the list. Currently locale settings for
    punctuation ignoring only implement english locale.

--- include/bayesian_webclass/dictionary.h
+++ include/bayesian_webclass/dictionary.h
@@ -1,23 +1,21 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
-#include <iomanip>
 #include <iostream>
-#include <sstream>
 #include <fstream>
 #include <string>
 #include <vector>
 
 class Dictionary {
-public:
-    Dictionary();
-    ~Dictionary();
-    void write_str_to_file(std::string filename, std::string str);
+    public:
+        Dictionary();
+        ~Dictionary();
+        void write_str_to_file(std::string filename, std::string str);
+        void fetch_from_file(std::string filename);  // fetches a list of words from given file with one word per line assumed
+        int compare(const std::string& filename);
 
-	void fetch_from_file(std::string filename); // fetches a list of words from given file with one word per line assumed
-
-private:
-    std::vector<std::string> _word_list;
+    private:
+        std::vector<std::string> _word_list;
 };
 
 
--- src/dictionary.cpp
+++ src/dictionary.cpp
@@ -1,5 +1,16 @@
 #include <bayesian_webclass/dictionary.h>
 
+struct letter_only : std::ctype<char> {
+    letter_only() : std::ctype<char>(get_table()) {}
+
+    static std::ctype_base::mask const* get_table() {
+        static std::vector<std::ctype_base::mask>
+            rc(std::ctype<char>::table_size,std::ctype_base::space);
+
+        std::fill(&rc['A'], &rc['z'+1], std::ctype_base::alpha);
+        return &rc[0];
+    }
+};
 
 void Dictionary::write_str_to_file(std::string filename, std::string str) {
     std::ofstream myfile;
@@ -8,14 +19,14 @@ void Dictionary::write_str_to_file(std::string filename, std::string str) {
     myfile.close();
 }
 
-std::vector<std::string> Dictionary::fetch_from_file(std::string filename) {
+void Dictionary::fetch_from_file(std::string filename) {
     std::vector<std::string> word_list; //stores addresses of html
     std::string line;
     std::ifstream word_list_file (filename); //in every line should be other http address
     if (word_list_file.is_open()) {
         while (! word_list_file.eof()) {
             std::getline(word_list_file,line,"="); // delimitation with "=", line format is "word=freq"
-                                                   // only word is needed here
+            // only word is needed here
             word_list.push_back(line);
         }
         word_list_file.close();
@@ -23,4 +34,22 @@ std::vector<std::string> Dictionary::fetch_from_file(std::string filename) {
         std::cout << "Failed to open the file." << std::endl;
     }
     this->_word_list = word_list;
+}
+
+int Dictionary::compare(const std::string& filename) {
+    std::map<std::string, int> wordCnt;
+    std::ifstream input;
+    input.imbue(std::locale(std::locale(), new letter_only()));
+    input.open(filename);
+    std::string word;
+
+    while(input >> word) {
+        ++wordCnt[word];
+    }
+
+    int matchedCnt;
+    for(auto w : _word_list) {
+        matchedCnt += wordCnt[w]
+    }
+    return matchedCnt;
 }
