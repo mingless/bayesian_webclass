9e8f80a24ac959c7863070385e612c1e464150bf
Alek Piotrowski <piotrowskialek@icloud.com>
2017-01-22 17:31:37 +0100

    fixed some bugs

--- bioweb/calc/src/calc.hpp
+++ bioweb/calc/src/calc.hpp
@@ -24,32 +24,5 @@ CALC_DLL( std::string ping(); )	//test function???
 #include <mt4cpp/Scheduler.hpp>
 #include <mt4cpp/CommandHistory.hpp>
 
-/**
-   singleton, active object design pattern
-*/
-// class CALC_DLL( CommandManager ) {
-// public:
-// 	static CommandManager& getInstance();
-
-// 	/** execute testing command (this command is finished after 0.2 s) */
-// 	mt4cpp::CommandID runTickCommand( int steeps = 10 );
-
-// 	/** keys for stored commands */
-// 	std::vector<mt4cpp::CommandID> commandKeys() const;
-
-// 	* description for command with given id 
-// 	mt4cpp::CommandDesc findCommandDesc(mt4cpp::CommandID id) const;
-
-// 	void clearHistory();
-// private:
-// 	CommandManager();
-
-// 	mt4cpp::Scheduler scheduler_;
-// 	mt4cpp::CommandHistory history_;
-
-// 	CommandManager(const CommandManager&) = delete;
-// 	CommandManager operator=(const CommandManager&) = delete;
-// };
-
 
 #endif //CALC_HPP
